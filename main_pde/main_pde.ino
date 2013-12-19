cc0e575cd03976011b1a5c42e7f118ac95fad438
Alex Gaudio <adgaudio@gmail.com>
2013-12-19 06:43:09 -0500

    in prog.  making yesterday's code work

--- main_pde/main_pde.ino
+++ main_pde/main_pde.ino
@@ -5,14 +5,17 @@ const int X_STEP_PIN = 5;
 const int Y_STEP_PIN = 6;
 const int X_DIR_PIN = 4;
 const int X_STEPS_PER_REV = 200 * 32;
-const int PIN_MAP = {X_STEP_PIN, Y_STEP_PIN};
-const int NUM_PINS = sizeof(PIN_MAP) / sizeof(PIN_MAP[0]);
-const int PWM_DIVISOR = 8; // arduino default = 64
+int PIN_MAP[] = {X_STEP_PIN, Y_STEP_PIN};
+int NUM_PINS = sizeof(PIN_MAP) / sizeof(PIN_MAP[0]);
+
+// Custom PWM and Watchdog config
+int PWM_DIVISOR = 8; // arduino default = 64
 CustomPwm custpwm;
+int WDT_DELAY = 8 * ARDUINO_DEFAULT_PWM_DIVISOR / (float) (PWM_DIVISOR); // how long before watchdog resets arduino
 
 
-void fail(msg="<unknown error>") {
-  Serial.println("Fail: " + msg)
+void fail(String msg="<unknown error>") {
+  Serial.println("Fail: " + msg);
   // force hang.
   while (1) { }
 }
@@ -21,13 +24,14 @@ void fail(msg="<unknown error>") {
 void setup() {
   // watchdog: reset after X seconds if counter not reset
   // --> because we're setting a custom pwm, this shortens the delay a lot!
-  wdt_enable (WDTO_5S);
+  wdt_enable(WDTO_8S);
 
   // serial stuff
   Serial.begin(9600); 
-  /*while (!Serial) {*/
-    /*; // wait for serial port to connect. Needed for Leonardo only*/
-  /*}*/
+  //while (!Serial) {
+  //  ; // wait for serial port to connect. Needed for Leonardo only
+  //}
+  Serial.println("Hello World");
 
   pinMode(X_STEP_PIN, OUTPUT);
   pinMode(X_DIR_PIN, OUTPUT);
@@ -40,13 +44,14 @@ void setup() {
 void loop() {
   wdt_reset (); // reset watchdog counter
 
-  int step_pins[] = PIN_MAP;
-  step(step_pins, 1000);
-  custpwm.wait(1000);
+  int step_pins[] = {400, 400};
+  Serial.println("stepping...");
+  step(step_pins, 500);
+  custpwm.wait(500);
 }
 
 
-void step(int[NUM_PINS] steps_per_pin, int millisecs) {
+void step(int steps_per_pin[], int millisecs) {
   /*
   Given a number of steps per pin,
   For each pin,
@@ -58,16 +63,21 @@ void step(int[NUM_PINS] steps_per_pin, int millisecs) {
   int total_num_steps = lcm(steps_per_pin, NUM_PINS);
 
   float delay_between_steps = (float)millisecs / (float)total_num_steps;
-  if (delay_between_steps > WDTO_5S) {
-    fail("step(): Too much delay between motor pulses. Try increasing the" +
+  if (delay_between_steps > WDT_DELAY) {
+    fail("step(): Too much delay between motor pulses. Try increasing the"
          " steps_per_pin values or reducing the total travel time");
   }
+
   // for each pin, a pulse comes every cnt steps
   int counter_max[NUM_PINS];
-  int counters[NUM_PINS] = {0};
+  int counters[NUM_PINS];
   for (int i=0; i<NUM_PINS ; i++) {
     counter_max[i] = total_num_steps / steps_per_pin[i];
+    counters[i] = 0; // hack: can't figur eout how to initialize properly
   }
+  Serial.println(counter_max[0]);
+  Serial.println(counters[0]);
+  Serial.println(total_num_steps);
 
   for (int step=0; step<=total_num_steps; step++) {
 
@@ -75,6 +85,7 @@ void step(int[NUM_PINS] steps_per_pin, int millisecs) {
     for (int j=0; j<= NUM_PINS ; j++) {
       if (++counters[j] >= counter_max[j]) {
         counters[j] = 0;
+        //Serial.println(PIN_MAP[j]);
         digitalWrite(PIN_MAP[j], HIGH);
         digitalWrite(PIN_MAP[j], LOW);
       }
