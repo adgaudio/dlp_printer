e4f3c567f4c3bf56d2f30b6bf14d73ebfc87d29c
Alex Gaudio <adgaudio@gmail.com>
2014-01-05 14:51:36 -0500

    fix numerical problems

--- main_pde/main_pde.ino
+++ main_pde/main_pde.ino
@@ -1,25 +1,29 @@
 #include "avr/wdt.h"  // watchdog resets avr chip if it hangs
 
-const int X_STEP_PIN = 5;
-const int Y_STEP_PIN = 6;
-const int X_DIR_PIN = 4;
+// stepper motor config
+const int _X_DIR_PIN = 4;
+const int _X_STEP_PIN = 5;
+const int _Y_DIR_PIN = 6;
+const int _Y_STEP_PIN = 7;
+const int NUM_PINS = 2; //sizeof(PIN_MAP) / sizeof(PIN_MAP[0]);
 const int X_STEPS_PER_REV = 200 * 32;
-int PIN_MAP[] = {X_STEP_PIN, Y_STEP_PIN};
-int NUM_PINS = 1; //sizeof(PIN_MAP) / sizeof(PIN_MAP[0]);
+// derived stepper config
+int PIN_MAP[] = {_X_STEP_PIN, _Y_STEP_PIN}; // list the pwm pins for each stepper motor
+int PIN_DIR_MAP[] = {_X_DIR_PIN, _Y_DIR_PIN}; // list the dir pins for each stepper motor
 
-int WDT_DELAY = 8000;// * ARDUINO_DEFAULT_PWM_DIVISOR / (float) (PWM_DIVISOR); // how long before watchdog resets arduino
+// reset arduino after 8 seconds.
+const int WDT_DELAY = 8000000;  // consider this non-configurable
 
 
 void fail(String msg="<unknown error>") {
   Serial.println("Fail: " + msg);
-  // force hang.
+  // force hang
   while (1) { }
 }
 
 
 void setup() {
   // watchdog: reset after X seconds if counter not reset
-  // --> because we're setting a custom pwm, this shortens the delay a lot!
   wdt_enable(WDTO_8S);
 
   // serial stuff
@@ -29,46 +33,50 @@ void setup() {
   //}
   Serial.println("Hello World");
 
-  pinMode(X_STEP_PIN, OUTPUT);
-  pinMode(X_DIR_PIN, OUTPUT);
+  for (int i=0 ; i<NUM_PINS; i++) {
+    pinMode(PIN_MAP[i], OUTPUT);
+    pinMode(PIN_DIR_MAP[i], OUTPUT);
+  }
 }
 
 
 void loop() {
   wdt_reset (); // reset watchdog counter
 
-  int step_pins[] = {400, 500};
-  step(step_pins, 500);
+  unsigned int step_pins[] = {800, 400};
+  step(step_pins, 500000);
   delay(500);
 }
 
 
-void step(int steps_per_pin[], int millisecs) {
+void step(unsigned int steps_per_pin[], unsigned int microsecs) {
   /*
   Given a number of steps per pin,
   For each pin,
-  pulse a high-low sequence once per step over the course of X `millisecs`
+  pulse a high-low sequence once per step over the course of X `microsecs`
 
   Pulses happen as close to concurrently as possible, and pins are identified
   by index in the array
   */
-  int total_num_steps = lcm(steps_per_pin, NUM_PINS);
+  unsigned int total_num_steps = lcm(steps_per_pin, NUM_PINS);
 
-  float delay_between_steps = (float)millisecs * 1000.0 / (float)total_num_steps;
+  unsigned int delay_between_steps = microsecs / total_num_steps - 1;
   if (delay_between_steps > WDT_DELAY) {
     fail("step(): Too much delay between motor pulses. Try increasing the"
          " steps_per_pin values or reducing the total travel time");
+  } else if (delay_between_steps < 1) {
+    fail("step(): Not enough delay between motor pulses.  Try decreasing the"
+         " steps_per_pin values or increasing the total travel time");
   }
 
   // for each pin, a pulse comes every cnt steps
-  int counter_max[NUM_PINS];
-  int counters[NUM_PINS];
+  unsigned int counter_max[NUM_PINS];
+  unsigned int counters[NUM_PINS];
   for (int i=0; i<NUM_PINS ; i++) {
     counter_max[i] = total_num_steps / steps_per_pin[i];
     counters[i] = 0; // hack: can't figur eout how to initialize properly
   }
   for (int step=0; step<=total_num_steps; step++) {
-
     // pulse the pins on each step
     for (int j=0; j < NUM_PINS ; j++) {
       if (++counters[j] >= counter_max[j]) {
@@ -80,7 +88,7 @@ void step(int steps_per_pin[], int millisecs) {
         digitalWrite(PIN_MAP[j], LOW);
       }
     }
-    delayMicroseconds(delay_between_steps-1);
+    delayMicroseconds(delay_between_steps);
     wdt_reset(); // reset watchdog counter
   }
 }
@@ -98,15 +106,16 @@ int gcd(int a, int b) {
 }
 
 
-int lcm(int ints[], int num_ints) {
+int lcm(unsigned int ints[], int num_ints) {
   /* Find the Lowest Common Multiple of an array of ints
    *
    * lcm = a*b / gcd(a, b)
    * lcm_of_many_ints = reduce(lcm, ints)
    */
-  int lcm_val = ints[0];
+
+  unsigned int lcm_val = ints[0];
   for (int i=1 ; i < num_ints ; i++) {
-    lcm_val = lcm_val * ints[i] / gcd(lcm_val, ints[i]);
+    lcm_val = lcm_val * (ints[i] / gcd(lcm_val, ints[i]));
   }
   return lcm_val;
 }
